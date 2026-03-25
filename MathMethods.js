159498cff65b504a34d22b3468f6198b4b1f6926
aashiya7866 <aashiya.khan8377@gmail.com>
2026-03-25 19:56:35 +0530

    number swap programe

---
+++ MathMethods.js
@@ -0,0 +1,28 @@
+let num1=9.2
+console.log(num1)
+
+console.log(Math.floor(num1))
+console.log(Math.ceil(num1))
+
+
+let num22=Math.floor(2.9)
+console.log("thi is num2 value "+num22)
+
+let num2 = 9.235568
+
+let num3 = 9.2344
+
+console.log(num2.toFixed(2))
+// console.log(num3.toFixed(2))
+
+console.log(Math.random())
+
+console.log(Math.random()*100)
+
+
+console.log(Math.floor(Math.random()*100)+1) 
+
+
+console.log(Math.ceil(Math.random()*100)+1) 
+let randomVal = Math.random(); 
+console.log(Math.floor(randomVal * 10) + 5);
---
+++ swapNumberPrograme.js
@@ -0,0 +1,7 @@
+let a=5
+let b=10
+
+let temp =a///////////////temp =5
+a=b////a=10
+b=temp//b=5
+console.log(a,b)//10,5
